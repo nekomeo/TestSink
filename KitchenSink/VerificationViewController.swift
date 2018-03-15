883eecee3fcdb3056a4da647cfdc84062ba775f5
nekomeo <linh.tu545@gmail.com>
2018-03-15 12:09:25 -0700

    Remove code not being used

--- KitchenSink/VerificationViewController.swift
+++ KitchenSink/VerificationViewController.swift
@@ -124,33 +124,21 @@ class VerificationViewController: UIViewController {
         codeStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
         code1TextField = UITextField(rounded: true)
-//        code1TextField.tag = 11
-//        code1TextField.addTarget(self, action: #selector(codeChanged(_:)), for: .editingChanged)
         codeStackView.addArrangedSubview(code1TextField)
         
         code2TextField = UITextField(rounded: true)
-//        code1TextField.tag = 12
-//        code2TextField.addTarget(self, action: #selector(codeChanged(_:)), for: .editingChanged)
         codeStackView.addArrangedSubview(code2TextField)
         
         code3TextField = UITextField(rounded: true)
-//        code1TextField.tag = 13
-//        code3TextField.addTarget(self, action: #selector(codeChanged(_:)), for: .editingChanged)
         codeStackView.addArrangedSubview(code3TextField)
         
         code4TextField = UITextField(rounded: true)
-//        code1TextField.tag = 14
-//        code4TextField.addTarget(self, action: #selector(codeChanged(_:)), for: .editingChanged)
         codeStackView.addArrangedSubview(code4TextField)
         
         code5TextField = UITextField(rounded: true)
-//        code1TextField.tag = 15
-//        code5TextField.addTarget(self, action: #selector(codeChanged(_:)), for: .editingChanged)
         codeStackView.addArrangedSubview(code5TextField)
         
         code6TextField = UITextField(rounded: true)
-//        code1TextField.tag = 16
-//        code6TextField.addTarget(self, action: #selector(codeChanged(_:)), for: .editingChanged)
         codeStackView.addArrangedSubview(code6TextField)
         
         errorLabel = UILabel()
@@ -159,7 +147,6 @@ class VerificationViewController: UIViewController {
         errorLabel.topAnchor.constraint(equalTo: codeStackView.bottomAnchor, constant: 30.0).isActive = true
         errorLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
-        // @objc func textFieldDidChange(textField: UITextField)
         code1TextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
         code2TextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
         code3TextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
@@ -167,21 +154,6 @@ class VerificationViewController: UIViewController {
         code5TextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
         code6TextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
         
-        //func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool
-//        code1TextField.addTarget(self, action: #selector(textFieldChanged(textField:shouldChangeCharactersInRange:replacementString:)), for: .editingChanged)
-//        code2TextField.addTarget(self, action: #selector(textField(_:shouldChangeCharactersIn:replacementString:)), for: .editingChanged)
-//        code3TextField.addTarget(self, action: #selector(textField(_:shouldChangeCharactersIn:replacementString:)), for: .editingChanged)
-//        code4TextField.addTarget(self, action: #selector(textField(_:shouldChangeCharactersIn:replacementString:)), for: .editingChanged)
-//        code5TextField.addTarget(self, action: #selector(textField(_:shouldChangeCharactersIn:replacementString:)), for: .editingChanged)
-//        code6TextField.addTarget(self, action: #selector(textField(_:shouldChangeCharactersIn:replacementString:)), for: .editingChanged)
-        
-        // func textFieldHasChange(textField: UITextField)
-//        code1TextField.addTarget(self, action: #selector(textFieldHasChange(textField:)), for: .editingChanged)
-//        code2TextField.addTarget(self, action: #selector(textFieldHasChange(textField:)), for: .editingChanged)
-//        code3TextField.addTarget(self, action: #selector(textFieldHasChange(textField:)), for: .editingChanged)
-//        code4TextField.addTarget(self, action: #selector(textFieldHasChange(textField:)), for: .editingChanged)
-//        code5TextField.addTarget(self, action: #selector(textFieldHasChange(textField:)), for: .editingChanged)
-//        code6TextField.addTarget(self, action: #selector(textFieldHasChange(textField:)), for: .editingChanged)
     }
     
     /*
@@ -221,7 +193,6 @@ class VerificationViewController: UIViewController {
                 break
             }
         }
-//        else if ((textField.text?.count)! >= 1  && text?.count == 0) {
         else if text?.utf16.count == 0 {
             let textField = ""
             switch textField {
@@ -343,122 +314,12 @@ class VerificationViewController: UIViewController {
 //        }
 //    }
     
-//    @objc func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-//        // On inputing value to textfield
-//        if ((textField.text?.count)! < 1  && string.count > 0) {
-//            if (textField == code1TextField) {
-//                code2TextField.becomeFirstResponder()
-//            }
-//            if (textField == code2TextField) {
-//                code3TextField.becomeFirstResponder()
-//            }
-//            if (textField == code3TextField) {
-//                code4TextField.becomeFirstResponder()
-//            }
-//            if (textField == code4TextField) {
-//                code5TextField.becomeFirstResponder()
-//            }
-//            if (textField == code5TextField) {
-//                code6TextField.becomeFirstResponder()
-//            }
-//            textField.text = string
-//            return false
-//
-//        } else if ((textField.text?.count)! >= 1  && string.count == 0) {
-//            // on deleting value from Textfield
-//            if (textField == code2TextField) {
-//                code1TextField.becomeFirstResponder()
-//            }
-//            if (textField == code3TextField) {
-//                code2TextField.becomeFirstResponder()
-//            }
-//            if (textField == code4TextField) {
-//                code3TextField.becomeFirstResponder()
-//            }
-//            if (textField == code5TextField) {
-//                code4TextField.becomeFirstResponder()
-//            }
-//            if (textField == code6TextField) {
-//                code5TextField.becomeFirstResponder()
-//            }
-//            textField.text = ""
-//            return false
-//        } else if ((textField.text?.count)! >= 1 ) {
-//            textField.text = string
-//            return false
-//        }
-//        return true
-//    }
     
     @objc func dismissButtonPressed() {
         self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
     }
     
-//    @objc func codeChanged(_ sender: UITextField) {
-//        guard let text = sender.text else { return }
-//
-//        let lastChar = text.suffix(1)
-//        sender.text = String(lastChar)
-//
-//        if sender.tag >= 11 && sender.tag <= 15 {
-//            let nextTextField = sender.superview?.viewWithTag(sender.tag + 1)
-//            nextTextField?.becomeFirstResponder()
-//        }
-//
-////        displayNoValidation()
-//    }
-    
-//    func getInputCode() -> String {
-//        let firstChar = code1TextField.text ?? ""
-//        let secondChar = code2TextField.text ?? ""
-//        let thirdChar = code3TextField.text ?? ""
-//        let fourthChar = code4TextField.text ?? ""
-//        let fifthChar = code5TextField.text ?? ""
-//        let sixthChar = code6TextField.text ?? ""
-//
-//        return "\(firstChar)\(secondChar)\(thirdChar)\(fourthChar)\(fifthChar)\(sixthChar)"
-//    }
-    
-//    func displayValidationError(message: String) {
-//
-//        // Show error message
-//        errorLabel.text = message
-//
-//        setTextFieldStyle(textField: code1TextField, errorStyle: true)
-//        setTextFieldStyle(textField: code2TextField, errorStyle: true)
-//        setTextFieldStyle(textField: code3TextField, errorStyle: true)
-//        setTextFieldStyle(textField: code4TextField, errorStyle: true)
-//        setTextFieldStyle(textField: code5TextField, errorStyle: true)
-//        setTextFieldStyle(textField: code6TextField, errorStyle: true)
-//
-//        code1TextField.becomeFirstResponder()
-//    }
-//
-//    func displayNoValidation() {
-//
-//        // Clear error message
-//        errorLabel.text = ""
-//
-//        setTextFieldStyle(textField: code1TextField, errorStyle: false)
-//        setTextFieldStyle(textField: code2TextField, errorStyle: false)
-//        setTextFieldStyle(textField: code3TextField, errorStyle: false)
-//        setTextFieldStyle(textField: code4TextField, errorStyle: false)
-//        setTextFieldStyle(textField: code5TextField, errorStyle: false)
-//        setTextFieldStyle(textField: code6TextField, errorStyle: true)
-//    }
-    
-//    func setTextFieldStyle(textField: UITextField, errorStyle: Bool) {
-//        textField.autocorrectionType = UITextAutocorrectionType.no
-//        textField.layer.borderWidth = 1.0
-//        textField.layer.cornerRadius = 20.0
-//        textField.layer.borderColor = errorStyle ? UIColor.red.cgColor : UIColor(red: 105/255, green: 128/255, blue: 151/255, alpha: 1.0).cgColor
-//
-//        // Maybe there is a simpler way to change the color of the current text...
-//        let tempText = textField.text
-//        textField.text = ""
-//        textField.textColor = errorStyle ? UIColor.red : UIColor(red: 105/255, green: 128/255, blue: 151/255, alpha: 1.0)
-//        textField.text = tempText
-//    }
+
     
     //MARK: - Editing Passcode
     func textFieldDidBeginEditing(_ textField: UITextField) {
@@ -467,54 +328,6 @@ class VerificationViewController: UIViewController {
         }
     }
     
-//    @objc func textFieldHasChange(textField: UITextField) {
-//
-//        let text = textField.text
-//
-//        if text?.utf16.count == 1 {
-//            switch textField {
-//            case code1TextField:
-//                if code2TextField.text?.utf16.count == 1 {
-//                    code1TextField.resignFirstResponder()
-//                }
-//                else {
-//                    code2TextField.becomeFirstResponder()
-//                }
-//            case code2TextField:
-//                if code3TextField.text?.utf16.count == 1 {
-//                    code2TextField.resignFirstResponder()
-//                }
-//                else {
-//                    code3TextField.becomeFirstResponder()
-//                }
-//            case code3TextField:
-//                if code4TextField.text?.utf16.count == 1 {
-//                    code3TextField.resignFirstResponder()
-//                }
-//                else {
-//                    code4TextField.becomeFirstResponder()
-//                }
-//            case code4TextField:
-//                if code5TextField.text?.utf16.count == 1 {
-//                    code4TextField.resignFirstResponder()
-//                }
-//                else {
-//                    code5TextField.becomeFirstResponder()
-//                }
-//            case code5TextField:
-//                if code6TextField.text?.utf16.count == 1 {
-//                    code5TextField.resignFirstResponder()
-//                }
-//                else {
-//                    code6TextField.becomeFirstResponder()
-//                }
-//            case code6TextField:
-//                code6TextField.resignFirstResponder()
-//            default:
-//                break
-//            }
-//        }
-//    }
 
 }
 
